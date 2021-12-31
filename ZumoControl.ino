d0d2a6c3e7b9275289d14bb5d784e6eb96e61248
Ben Sinyard <bennybobber22@gmail.com>
2021-12-31 15:51:38 +0000

    began working on manual mode for the Zumo movement

--- ZumoControl.ino
+++ ZumoControl.ino
@@ -1,6 +1,6 @@
 // External libraries
 
-#include <ZumoMotors.h>
+#include <Zumo32U4Motors.h>
 #include <ZumoBuzzer.h>
 #include <QTRSensors.h>
 #include <ZumoReflectanceSensorArray.h>
@@ -8,15 +8,15 @@
 // Global Variables
 
 #define TURN_SPEED 100
-#define CALIBRATE_SPEED 200
-#define MOVE_SPEED 150
+#define CALIBRATE_SPEED 150
+#define MOVE_SPEED 200
 
 #define LED 13
 #define HALT 0
 #define NO_SENSORS 5
 
 //setup library class objects
-ZumoMotors motors;
+Zumo32U4Motors motors;
 ZumoReflectanceSensorArray sensors(QTR_NO_EMITTER_PIN);
 ZumoBuzzer buzzer;
 
@@ -34,7 +34,7 @@ void setup() {
     {
         incomingByte = (char) Serial.read();
     }
-
+    calibrateZumo();
     robotMode = 0;
 }
 
@@ -51,7 +51,67 @@ void loop() {
 }
 
 void manualControl() {
+    incomingByte = Serial.read();
 
+    // Check what the user wants to do.
+    switch (incomingByte) {
+        case 'w':
+            Serial.println("Zumo Moving Forward...");
+            motors.setSpeeds(MOVE_SPEED, MOVE_SPEED);
+            delay(150);
+            motors.setSpeeds(HALT, HALT);
+            break;
+        case 'a':
+            Serial.println("Zumo Turning Left...");
+            // Defining each motor to make it clearer which motor is moving forward, and which is moving backwards.
+            motors.setLeftSpeed(-MOVE_SPEED);
+            motors.setRightSpeed(MOVE_SPEED);
+            delay(150);
+            motors.setSpeeds(HALT, HALT);
+            break;
+        
+        case 'd':
+            // Defining each motor to make it clearer which motor is moving forward, and which is moving backwards.
+            Serial.println("Zumo Turning Right...");
+            motors.setLeftSpeed(MOVE_SPEED);
+            motors.setRightSpeed(-MOVE_SPEED);
+            delay(150);
+            motors.setSpeeds(HALT, HALT);
+            break;
+        
+        case 's':
+            Serial.println("Zumo Reversing...");
+            motors.setSpeeds(-MOVE_SPEED, -MOVE_SPEED);
+            delay(150);
+            motors.setSpeeds(HALT, HALT);
+            break;
+        case 'e':
+            Serial.println("Zumo turning right 90 degrees...");
+            motors.setSpeeds(MOVE_SPEED, -TURN_SPEED);
+            delay(1050);
+            motors.setSpeeds(HALT, HALT);
+            break;
+        case 'q':
+            Serial.println("Zumo turning left 90 degrees...");
+            motors.setSpeeds(-TURN_SPEED, MOVE_SPEED);
+            delay(1050);
+            motors.setSpeeds(HALT, HALT);
+            break;
+        case 'h':
+            Serial.println("Tooting The Horn!");
+            buzzer.playNote(NOTE_A_SHARP(5), 250, 90);
+            break;
+        case 'z':
+            Serial.println("Emergency Stop Initiated!");
+            motors.setSpeeds(HALT, HALT);
+            break;
+        case '1':
+            Serial.println("Auto pilot activated");
+            buzzer.play(">g32>>c32");
+            robotMode = 1;
+            break;
+
+    }
 };
 
 void autoMode() {
