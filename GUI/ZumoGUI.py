9fc83513947aa7021e272834ad37d0695077ccfe
Ben Sinyard <bennybobber22@gmail.com>
2022-01-12 18:31:22 +0000

    Final additions to the Zumo Program and GUI

--- GUI/ZumoGUI.py
+++ GUI/ZumoGUI.py
@@ -34,6 +34,9 @@ text = smallfont.render('quit', True, white)
 
 key=''
 font   = pygame.font.SysFont("consolas", 25, True)
+
+manualMode = True
+automaticMode = False
 while True:
     for event in pygame.event.get():
 
@@ -46,9 +49,11 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if 500 <= mouse[0] <= 500+140 and 600 <= mouse[1] <= 600+40:
                 pygame.quit()
+            if 60 <=mouse[0] <= 100 + 425 and 600 <= mouse[1] <= 600+40:
+                manualMode = not manualMode
+                automaticMode = False;
     time.sleep(0.02)
     if key != '':
-        print(key)
         ser.write(chr(key).encode())
     if key != 119 and key != 97 and key != 115 and key != 100:
         key = ''
@@ -63,10 +68,26 @@ while True:
     
     msg_string = CURHR
     msg_text = font.render(msg_string, True, red)
-    screen.blit(smallfont.render(msg_string, True, red), (25, 350))
+    screen.blit(smallfont.render(msg_string, True, red), (25, 50))
     
     
-    
+    if manualMode:
+        screen.blit(smallfont.render('Use "wasd" to move the Zumo', True, black) , (10,200))
+        screen.blit(smallfont.render('Press "r" to log a room', True, black) , (10,250))
+        screen.blit(smallfont.render('Press "l" to search a room', True, black) , (10,300))
+        screen.blit(smallfont.render('Press "q" or "e" to turn 90 degrees (left, right)', True, black) , (10,350))
+        screen.blit(smallfont.render('Press "h" to sound the horn!', True, black) , (10,400))
+        screen.blit(smallfont.render('Press "z" emergency stop!', True, black) , (10,450))
+        screen.blit(smallfont.render('Press "1" to switch to auto mode.', True, black) , (10,500))
+        pygame.draw.rect(screen,red,[60,600,425,40])
+        screen.blit(smallfont.render('Show Automatic Instructions', True, white) , (70,600))
+    else:
+        screen.blit(smallfont.render('Press "z" emergency stop!', True, black) , (10,200))
+        screen.blit(smallfont.render('Press "r" stop zumo then "l" or "r" to intiate', True, black) , (10,250))
+        screen.blit(smallfont.render('a search of the room, which is done automatically.', True, black) , (10,300))
+        screen.blit(smallfont.render('At the end of the corridor, follow the instructions.', True, black) , (10,450))
+        pygame.draw.rect(screen,green,[60,600,425,40])
+        screen.blit(smallfont.render('Show Manual Instructions', True, white) , (70,600))
     
     # superimposing the text onto our button
     
--- ZumoControl.ino
+++ ZumoControl.ino
@@ -34,51 +34,51 @@
 #include <Zumo32U4Buzzer.h>
 #include <Zumo32U4ProximitySensors.h>
 #include <ZumoReflectanceSensorArray.h>
-#include <Zumo32U4IMU.h>
+#include <Zumo32U4Encoders.h>
 #include <Wire.h>
 // Global Variables
 
-#define TURN_SPEED 100
-#define CALIBRATE_SPEED 200
-#define MOVE_SPEED 200
-#define AUTO_SPEED 75
-#define CALIBRATION_SAMPLES 70 // Number of calibration samples to take for the compass
-#define DEVIATION_THRESHOLD 5 // How far compass reading can deviate
-
-#define LED 13
+// Movement based variables
+#define TURN_SPEED 250
+#define CALIBRATE_SPEED 100
+#define MOVE_SPEED 350
+#define AUTO_SPEED 125
 #define HALT 0
+
+// Sesnsor variables
 #define NO_SENSORS 3
-#define MAX_SONAR_DISTANCE 50
 #define LEFT false
 #define RIGHT true
-#define IR_THRESHOLD = 1
+#define IR_THRESHOLD 5
 
-//setup library class objects
+// setup library class objects
 Zumo32U4Motors motors;
 ZumoReflectanceSensorArray sensors(QTR_NO_EMITTER_PIN);
 Zumo32U4Buzzer buzzer;
-Zumo32U4IMU imu;
+Zumo32U4Encoders encoders;
 
-//setup general variables
-Zumo32U4IMU::vector<int16_t> m_max; // maximum magnetometer values, used for calibration
-Zumo32U4IMU::vector<int16_t> m_min; // minimum magnetometer values, used for calibration
+// setup general variables
 unsigned int line_sensor_values[NO_SENSORS];
 int robotMode;
 int noOfRooms = 0;
+int wallHitCount = 0;
 unsigned int calibrateData[NO_SENSORS]; // calibration data for the line sensors
-int leftValue = 0; // left value for the proximity sensor
-int rightValue = 0; // right value for the promixty sensor
-char incomingByte;
-int leftProximityCount;
-int rightProximityCount;
 int stopCount;
 int leftSpeed;
 int rightSpeed;
+char incomingByte;
+// To keep track of number of rooms, their position and if a person is inside.
 String rooms[10];
 String people[10];
 String room;
 bool sensDir = RIGHT; // last indication of the direction of an object
 
+/*
+	Setup the serial port we're going to use.
+    Get the byte and then wait for the user to press the calibrate button.
+    
+    Then we switch to the default robot mode of 0 (Manual)
+*/
 void setup() {
     Serial1.begin(9600);
     incomingByte = Serial1.read();
@@ -88,10 +88,11 @@ void setup() {
         incomingByte = (char) Serial1.read();
     }
     calibrateZumo();
-    calibrateCompass();
     robotMode = 0;
 }
-
+/*
+	Loops around, checking to see if the mode of the robot has changed.
+*/
 void loop() {
     switch(robotMode) {
         case 0:
@@ -102,6 +103,10 @@ void loop() {
             break;
     }
 }
+/*
+	Uses a switch statement to check how the robot should react
+    depending on the input given by the user.
+*/
 void manualControl() {
     //This will speed down the engine unless a command to speed up is received
     if(abs(rightSpeed)>0){rightSpeed=rightSpeed-rightSpeed/5;}
@@ -114,23 +119,16 @@ void manualControl() {
     switch (incomingByte) {
         case 'w':
             Serial1.println("Zumo Moving Forward...");
-
-            // motors.setLeftSpeed(MOVE_SPEED);
-            // motors.setRightSpeed(MOVE_SPEED);
             leftSpeed = MOVE_SPEED;
             rightSpeed = MOVE_SPEED;
             break;
         case 'a':
             Serial1.println("Zumo Turning Left...");
-            // Defining each motor to make it clearer which motor is moving forward, and which is moving backwards.
-            // motors.setLeftSpeed(-MOVE_SPEED);
-            // motors.setRightSpeed(MOVE_SPEED);
             leftSpeed = -MOVE_SPEED;
             rightSpeed = MOVE_SPEED;
             break;
         
         case 'd':
-            // Defining each motor to make it clearer which motor is moving forward, and which is moving backwards.
             Serial1.println("Zumo Turning Right...");
             leftSpeed = MOVE_SPEED;
             rightSpeed = -MOVE_SPEED;
@@ -158,6 +156,7 @@ void manualControl() {
             motors.setSpeeds(HALT, HALT);
             break;
         case 'r':
+            // Run logRoom function
             logRoom();
             break;
         case 'l':
@@ -165,9 +164,7 @@ void manualControl() {
             searchRoom();
             break;
         case '1':
-            // Change to auto mode and reset the proximity counts.
-            leftProximityCount = 0;
-            rightProximityCount = 0;
+            // Change to auto mode.
             Serial1.println("Auto pilot activated");
             buzzer.play(">g32>>c32");
             robotMode = 1;
@@ -175,8 +172,15 @@ void manualControl() {
             break;
     
     }
-    delay(5);
+    delay(3);
 };
+/*
+	Makes the Zumo turn and search a room depending on the last logged
+    room location.
+
+    Once the search is complete it reports back if anyone was found and the location
+    that the search took place before moving back to the corridor to continue the search.
+*/
 void searchRoom() {
     Serial1.println("Searching Room...");
     Zumo32U4ProximitySensors proxSensors;
@@ -193,9 +197,9 @@ void searchRoom() {
     }
     // Move zumo into the room.
     motors.setSpeeds(AUTO_SPEED, AUTO_SPEED);
-    delay(1100);
+    delay(450);
     motors.setSpeeds(HALT, HALT);
-    for (int i = 0; i < 160; i++)
+    for (int i = 0; i < 40; i++)
     {
         // Spin zumo around, and scan using the IR sensors
         proxSensors.read();
@@ -205,37 +209,45 @@ void searchRoom() {
         if ((i > 10 && i <= 30) || (i > 50 && i <= 70) || (i > 90 && i <= 110) ||  (i > 130 && i <= 150) )
         {
             // Turn Zumo Right
-             motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
+             motors.setSpeeds(-CALIBRATE_SPEED, CALIBRATE_SPEED);
 
 
         }
         else
         {
             // Turn Zumo Left
-            motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
+            motors.setSpeeds(CALIBRATE_SPEED, -CALIBRATE_SPEED);
         }
-        delay(40);
+        delay(60);
         //If a person is found, it's time to stop moving and inform the user
-        objectSeen = leftValue >= 5 || rightValue >= 5;
+        objectSeen = leftValue > IR_THRESHOLD || rightValue > IR_THRESHOLD;
         
     }
     motors.setSpeeds(HALT, HALT);
     //Stop the motors if there is nothing found
     if (objectSeen)
     {
-        Serial1.println("We have found a PERSON");
+        Serial1.print("Person FOUND in room: ");
+        Serial1.print(noOfRooms);
+        Serial1.print(" on the ");
+        Serial1.print(rooms[noOfRooms]);
         buzzer.playNote(NOTE_A_SHARP(5), 250, 90);
         people[noOfRooms] = "true";
     }
     else
     {
-        Serial.println("No PERSON found");
+        Serial1.print("No PERSON in room: ");
+        Serial1.print(noOfRooms);
+        Serial1.print(" on the ");
+        Serial1.print(rooms[noOfRooms]);
         people[noOfRooms] = "false";
     }
-    // Leave the room
+    // Wait 2 seconds to show message before leaving.
+    delay(2000);
     Serial.println("Returning to corridor");
+    // Reverse out of the room.
     motors.setSpeeds(-AUTO_SPEED, -AUTO_SPEED);
-    delay(900);
+    delay(450);
     motors.setSpeeds(HALT, HALT);
     if (rooms[noOfRooms] == "left")
     {
@@ -248,7 +260,23 @@ void searchRoom() {
 
 
 }
+/*
+	The auto mode for the Zumo, it will navigate down a corridor, avoiding all walls
+    as well as allowing the user to emergency stop OR log a room. It will continously check
+    the line sensors to see if:
+
+    The left sensor is over the line, it will attempt to turn right
+
+    The right sensor is over the line, it will attempt to turn left
+
+    The middle sensor is over the line OR the side sensors repeatedly hit a line, it will
+    cause the Zumo to stop and move into the junction function to check whether it needs to turn
+    to the left or right, or 180 degrees to go to the other end of the corridor. 
+
+    It will finally end the journey once it has completed all different junction parts.
+*/
 void autoMode() {
+    
     incomingByte = Serial1.read();
     sensors.read(line_sensor_values);
     if ( incomingByte == 'z' )
@@ -265,41 +293,55 @@ void autoMode() {
         searchRoom();
         robotMode = 1;
     }
-    else if ((line_sensor_values[1] > calibrateData[1] )) {
+    // Check if the Zumo has hit a wall.
+    else if ((wallHitCount >= 2) || (line_sensor_values[1] > calibrateData[1])) {
 
     // if the middle sensors detect line, reverse away then stop.
-    motors.setSpeeds(-TURN_SPEED, -TURN_SPEED);
-    delay(250);
+    motors.setSpeeds(-AUTO_SPEED, -AUTO_SPEED);
+    delay(125);
     motors.setSpeeds(HALT, HALT);
     Serial1.println("Wall Detected!");
-    Serial1.println("Manual Mode activated");
+    wallHitCount = 0;
     // We have reached the end of a corridor, so increase the stop count by 1.
     stopCount++;
     // Enter junction to perform turn
     junction();
     }
 
+    // Check if the right most sensor is over the line
     else if ((line_sensor_values[2] >= calibrateData[2]) && (line_sensor_values[1] < calibrateData[1]))
     {
         // If right most line sensor detects line move left.
-        // motors.setSpeeds(-TURN_SPEED, -TURN_SPEED);
         Serial1.println("Correcting left...");
-        // delay(200);
         motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
-        delay(250);
-        motors.setSpeeds(AUTO_SPEED, AUTO_SPEED);
+        delay(175);
         robotMode = 1;
+        wallHitCount++;
+        // Straighten the Zumo up since it's hit the far wall
+        if (wallHitCount > 1) {
+            motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
+            Serial1.println("Straightening Up!");
+            delay(325);
+        }
     }
+
+    // Check if the left most sensor is over the line
     else if ((line_sensor_values[0] >= calibrateData[0]) && (line_sensor_values[1] < calibrateData[1])) {
 
         // If left most line sensor detects line move right.
         // motors.setSpeeds(-TURN_SPEED, -TURN_SPEED);
         Serial1.println("Correcting right...");
-        //delay(200);
         motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
-        delay(250);
-        motors.setSpeeds(AUTO_SPEED, AUTO_SPEED);
+        delay(175);
         robotMode = 1;
+        wallHitCount++;
+        // Straighten the Zumo up since it's hit the far wall
+        if (wallHitCount > 1) {
+            motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
+            Serial1.println("Straightening Up!");
+            delay(325);
+            
+        }
     }
     else
     {
@@ -307,6 +349,7 @@ void autoMode() {
         Serial1.println("Going forward...");
         motors.setSpeeds(AUTO_SPEED, AUTO_SPEED);
         robotMode = 1;
+        wallHitCount = 0;
     }
 
 };
@@ -315,6 +358,7 @@ void autoMode() {
 */
 void calibrateZumo() {
     Serial1.println("Calibrating sensors...");
+    // Intiate the sensors with the correct pins to use.
     byte pins[] = {A0, A3, 12};
     sensors.init(pins, 3);
     for (int i = 0; i < 160; i++)
@@ -333,12 +377,12 @@ void calibrateZumo() {
         }
 
         sensors.calibrate();
-        delay(2.5);
+        delay(5);
     }
 
     for (int i = 0; i < NO_SENSORS; i++)
     {
-        //Get the maximums (lightest areas) then add 150 to add a margin of error.
+        //Get the maximums then add 150 to add a margin of error.
         calibrateData[i] = sensors.calibratedMaximumOn[i] + 200;
     }
     motors.setSpeeds(HALT, HALT);
@@ -373,7 +417,7 @@ void logRoom() {
     Serial1.print("Room Number: ");
     Serial1.print(noOfRooms);
     Serial1.print(" is on the ");
-    Serial1.println(rooms[noOfRooms]);
+    Serial1.print(rooms[noOfRooms]);
 };
 
 /*
@@ -434,6 +478,7 @@ void junction() {
     else if (stopCount == 3) {
         Serial1.println("We've reached the end of the journey.");
         robotMode = 0;
+        stopCount = 0;
     }
 
 }
@@ -442,158 +487,37 @@ void junction() {
  Uses the Zumo's compass to calculate a 90 degree turn from where the Zumo
  is currently facing.
 
- :PARAMS:
+ @PARAMS:
  Bool: direction. Tells the function which way the Zumo wants to turn 90 degrees. 
 
 */
 void turn90(bool direction) {
-    float heading, relative_heading;
-    int speed;
-    heading = averageHeading();
-    bool in_correct_pos = false;
-    float target_heading;
-    speed = 100;
-    target_heading = fmod(averageHeading() + 90, 360);
-    if (!direction)
-        target_heading = (averageHeading() - 90);
-        if (target_heading < 0)
-            target_heading = 360 + target_heading;
+    int countsLeft;
+    int countsRight;
 
-    while (!in_correct_pos)
-    {
-        heading = averageHeading();
-        relative_heading = relativeHeading(heading, target_heading);
-        Serial1.print("TH: ");
-        Serial1.print(target_heading);
-        Serial1.print("CH: ");
-        Serial1.print(heading);
-        if (abs(relative_heading) < DEVIATION_THRESHOLD)
-        {
-            motors.setSpeeds(HALT, HALT);
-            in_correct_pos = true;
-            Serial1.println("Turn Complete... ");
+    // Reset the encoders 
+    encoders.getCountsAndResetLeft();
+    encoders.getCountsAndResetRight();
+    countsLeft = 0;
+    countsRight = 0;
+
+    // Check the direction we want to turn
+    if (direction){
+        motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
+        while ((countsLeft <= 750)) {
+            countsLeft = encoders.getCountsLeft();
+            countsRight = encoders.getCountsRight();
         }
-        else
-        {
-            // To avoid overshooting, the closer the Zumo gets to the target
-            // heading, the slower it should turn. Set the motor speeds to a
-            // minimum base amount plus an additional variable amount based
-            // on the heading difference.
-
-            speed = 180*relative_heading/180;
-            Serial1.print("S: ");
-            
-            if (speed < 0)
-                speed -= TURN_SPEED;
-            else
-                speed += TURN_SPEED;
-            Serial1.print(speed);
-            motors.setSpeeds(speed, -speed);
-            Serial1.println();
-
-  }
-
     }
-}
-/*
-
- Calibrates the Compass on the Zumo by rotating and getting the readings.
- They're stored in a vectors for the maximum and minimum readings from the sensors.
-
-*/
-void calibrateCompass() {
-    unsigned char index;
-    Zumo32U4IMU::vector<int16_t> running_min = {32767, 32767, 32767}, running_max = {-32767, -32767, -32767};
-    Wire.begin();
-    // Initialize IMU
-    imu.init();
-
-    // Enables accelerometer and magnetometer
-    imu.enableDefault();
-    imu.configureForTurnSensing();
-
-    Serial.println("Calibrating Compass...");
-
-    // To calibrate the magnetometer, the Zumo spins to find the max/min
-    // magnetic vectors. This information is used to correct for offsets
-     // in the magnetometer data.
-    motors.setLeftSpeed(CALIBRATE_SPEED);
-    motors.setRightSpeed(-CALIBRATE_SPEED);
-
-    for(index = 0; index < CALIBRATION_SAMPLES; index ++)
-    {
-        // Take a reading of the magnetic vector and store it in compass.m
-        imu.readMag();
-
-        running_min.x = min(running_min.x, imu.m.x);
-        running_min.y = min(running_min.y, imu.m.y);
-
-        running_max.x = max(running_max.x, imu.m.x);
-        running_max.y = max(running_max.y, imu.m.y);
-
-        Serial.println(index);
-
-        delay(50);
+    else{
+       motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
+       while ((countsRight < 600)) {
+            countsLeft = encoders.getCountsLeft();
+            countsRight = encoders.getCountsRight();
+        }
     }
+    countsLeft = encoders.getCountsAndResetLeft();
+    countsRight = encoders.getCountsAndResetRight();
+    motors.setSpeeds(HALT, HALT);
 
-    motors.setLeftSpeed(0);
-    motors.setRightSpeed(0);
-
-    // Store calibrated values in m_max and m_min
-    m_max.x = running_max.x;
-    m_max.y = running_max.y;
-    m_min.x = running_min.x;
-    m_min.y = running_min.y;
-}
-
-// Converts x and y components of a vector to a heading in degrees.
-// This calculation assumes that the Zumo is always level.
-template <typename T> float heading(Zumo32U4IMU::vector<T> v)
-{
-  float x_scaled =  2.0*(float)(v.x - m_min.x) / (m_max.x - m_min.x) - 1.0;
-  float y_scaled =  2.0*(float)(v.y - m_min.y) / (m_max.y - m_min.y) - 1.0;
-
-  float angle = atan2(y_scaled, x_scaled)*180 / M_PI;
-  if (angle < 0)
-    angle += 360;
-  return angle;
-}
-
-// Gets the difference in degrees between each angle.
-float relativeHeading(float heading_from, float heading_to)
-{
-  float relative_heading = heading_to - heading_from;
-
-  // constrain to -180 to 180 degree range
-  if (relative_heading > 180)
-    relative_heading -= 360;
-  if (relative_heading < -180)
-    relative_heading += 360;
-
-  return relative_heading;
-}
-
-/*
-
- Get the average of 10 different measurements to help bypass
- any motors magnetic interference on the magnetometer.
-
- RETURN: Float variable
-
-*/
-float averageHeading()
-{
-  Zumo32U4IMU::vector<int32_t> avg = {0, 0, 0};
-
-  for(int i = 0; i < 10; i ++)
-  {
-    imu.readMag();
-    avg.x += imu.m.x;
-    avg.y += imu.m.y;
-  }
-  avg.x /= 10.0;
-  avg.y /= 10.0;
-
-  // avg is the average measure of the magnetic vector.
-  return heading(avg);
 }
